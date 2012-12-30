5b335b0270ddc2084fdde849f442c2fb94e7d829
yoschi <jorgeantunes.b@gmail.com>
2012-12-31 00:30:49 +0100

    starting point for some js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,23 @@
 $(window).load(function() {
-	console.log("yaay");
+	console.log('stuff');
+	animation = new Animation();
 });
+
+function Animation(){
+	this.canvas = document.getElementById('scene');
+	this.ctx    = this.canvas.getContext('2d');
+	console.log('stuff');
+
+	this.Display();
+}
+
+Animation.prototype.Display = function(){
+	this.ctx.fillStyle = "rgb(200, 0, 0)";
+	this.ctx.fillRect(10, 10, 55, 50);
+
+	this.ctx.beginPath();
+	this.ctx.moveTo(75,50);
+	this.ctx.lineTo(100,75);
+	this.ctx.lineTo(100,25);
+	this.ctx.fill();
+}
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Cookie
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Precompile *all* assets, except those that start with underscore
+    config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
   end
 end
